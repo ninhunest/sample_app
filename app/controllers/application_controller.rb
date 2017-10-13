373f30fbc277a09160ae5eb9dcb7961c0f12412b
nguyen phan quang ninh <nguyenninhquang@gmail.com>
2017-10-13 08:28:43 +0700

    Add a full_title helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   def hello
-    render html: 'hello, world!'
+  	render html: 'chua hieu lam'
   end
 end
